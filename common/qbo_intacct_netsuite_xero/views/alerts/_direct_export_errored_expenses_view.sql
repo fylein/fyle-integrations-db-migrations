10d5206bdf5fbab72fae9ba2a0e5a8d9ef48bd9b
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2025-01-22 15:57:54 +0530

    Feat: Add missed Views

---
+++ common/qbo_intacct_netsuite_xero/views/alerts/_direct_export_errored_expenses_view.sql
@@ -0,0 +1,75 @@
+DROP VIEW IF EXISTS _direct_export_errored_expenses_view;
+
+CREATE VIEW _direct_export_errored_expenses_view AS
+WITH prod_workspace_ids AS (
+    SELECT id
+    FROM prod_workspaces_view
+),
+errored_expenses_in_complete_state AS (
+    SELECT COUNT(*)  AS complete_expenses_error_count
+    FROM expenses
+    WHERE
+        workspace_id IN (SELECT id FROM prod_workspace_ids)
+        AND accounting_export_summary->>'state' not in ('COMPLETE', 'DELETED')
+        AND id IN (
+            SELECT expense_id
+            FROM expense_groups_expenses
+            WHERE expensegroup_id IN (
+                SELECT expense_group_id
+                FROM task_logs
+                WHERE status = 'COMPLETE'
+                AND workspace_id IN (SELECT id FROM prod_workspace_ids)
+            )
+        )
+),
+errored_expenses_in_error_state AS (
+    SELECT COUNT(*) AS error_expenses_error_count
+    FROM expenses
+    WHERE
+        workspace_id IN (SELECT id FROM prod_workspace_ids)
+        AND accounting_export_summary->>'state' not in ('ERROR', 'DELETED')
+        AND id IN (
+            SELECT expense_id
+            FROM expense_groups_expenses
+            WHERE expensegroup_id IN (
+                SELECT expense_group_id
+                FROM task_logs
+                WHERE status IN ('FAILED', 'FATAL')
+                AND workspace_id IN (SELECT id FROM prod_workspace_ids)
+            )
+        )
+),
+errored_expenses_in_inprogress_state AS (
+    SELECT COUNT(*) AS in_progress_expenses_error_count
+    FROM expenses
+    WHERE
+        workspace_id IN (SELECT id FROM prod_workspace_ids)
+        AND accounting_export_summary->>'state' not in ('IN_PROGRESS', 'DELETED')
+        AND id IN (
+            SELECT expense_id
+            FROM expense_groups_expenses
+            WHERE expensegroup_id IN (
+                SELECT expense_group_id
+                FROM task_logs
+                WHERE status = 'IN_PROGRESS'
+                AND workspace_id IN (SELECT id FROM prod_workspace_ids)
+            )
+        )
+),
+not_synced_to_platform AS (
+    SELECT COUNT(*) AS not_synced_expenses_count
+    FROM expenses
+    WHERE
+        workspace_id IN (SELECT id FROM prod_workspace_ids)
+        AND accounting_export_summary->>'synced' = 'false'
+)
+SELECT 
+    complete_expenses_error_count,
+    error_expenses_error_count,
+    in_progress_expenses_error_count,
+    not_synced_expenses_count
+FROM 
+    errored_expenses_in_complete_state,
+    errored_expenses_in_error_state,
+    errored_expenses_in_inprogress_state,
+    not_synced_to_platform;
---
+++ netsuite/views/extended_expenses_view.sql
@@ -0,0 +1,68 @@
+drop view if exists extended_expenses_view;
+
+create or replace view extended_expenses_view as
+
+select 
+    e.id,
+    e.employee_email,
+    e.employee_name,
+    e.category,
+    e.sub_category,
+    e.project,
+    e.org_id,
+    e.expense_id,
+    e.expense_number,
+    e.claim_number,
+    e.amount,
+    e.currency,
+    e.foreign_amount,
+    e.foreign_currency,
+    e.tax_amount,
+    e.tax_group_id,
+    e.settlement_id,
+    e.reimbursable,
+    e.billable,
+    -- e.exported,
+    e.state,
+    e.vendor,
+    e.cost_center,
+    e.purpose,
+    e.report_id,
+    e.report_title,
+    e.corporate_card_id,
+    -- e.bank_transaction_id,
+    e.file_ids,
+    e.spent_at,
+    e.approved_at,
+    e.posted_at,
+    e.expense_created_at,
+    e.expense_updated_at,
+    e.created_at,
+    e.updated_at,
+    e.fund_source,
+    e.verified_at,
+    e.custom_properties,
+    e.previous_export_state,
+    e.accounting_export_summary,
+    e.paid_on_netsuite,
+    e.paid_on_fyle,
+    -- e.payment_number,
+    e.is_skipped,
+    e.workspace_id,
+
+    eg.id as expense_group_id,
+    eg.employee_name as expense_group_employee_name,
+    eg.export_url as expense_group_export_url,
+    eg.description as expense_group_description,
+    eg.created_at as expense_group_created_at,
+    eg.updated_at as expense_group_updated_at,
+    eg.workspace_id as expense_group_workspace_id,
+    eg.fund_source as expense_group_fund_source,
+    eg.exported_at as expense_group_exported_at,
+    eg.response_logs as expense_group_response_logs
+
+from expenses e
+
+join expense_groups_expenses ege on ege.expense_id = e.id
+join expense_groups eg on eg.id = ege.expensegroup_id
+;
