df9b1b3105e93b592eb7bc663ffa30a31efb086d
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-03-03 13:30:59 +0530

    qbd alerts views

--- qbd/views/alerts/receive_request_queue_in_progress_response_received_tasks_view.sql
+++ qbd/views/alerts/receive_request_queue_in_progress_response_received_tasks_view.sql
@@ -1,12 +1,12 @@
-DROP VIEW IF EXISTS receive_request_queue_in_progress_response_received_tasks_view;
+DROP VIEW IF EXISTS receive_queue_in_progress_response_received_tasks_view;
 
-CREATE OR REPLACE VIEW receive_request_queue_in_progress_response_received_tasks_view AS 
+CREATE OR REPLACE VIEW receive_queue_in_progress_response_received_tasks_view AS 
 WITH receive_request_queue_in_progress_response_received_tasks AS (
-    SELECT COUNT(*) AS receive_request_queue_in_progress_response_received_tasks_count
+    SELECT COUNT(*) AS receive_queue_in_progress_response_received_tasks_count
     FROM qbd_receive_request_queue
     WHERE
         workspace_id IN (SELECT id FROM prod_workspaces_view)
         AND status IN ('IN_PROGRESS', 'RESPONSE_RECEIVED')
-        AND updated_at < (NOW() - INTERVAL '45 minutes')
+        AND updated_at BETWEEN NOW() - INTERVAL '24 hours' AND NOW() - INTERVAL '45 mins'
 )
 SELECT * FROM receive_request_queue_in_progress_response_received_tasks;
