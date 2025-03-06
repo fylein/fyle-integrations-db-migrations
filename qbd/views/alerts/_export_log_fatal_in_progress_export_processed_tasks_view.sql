918d33dd9f1a329328046d0b0d004fe79acc04d5
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-03-06 18:18:02 +0530

    fix: update qbd alert views

--- qbd/views/alerts/_export_log_fatal_in_progress_export_processed_tasks_view.sql
+++ qbd/views/alerts/_export_log_fatal_in_progress_export_processed_tasks_view.sql
@@ -2,11 +2,13 @@ DROP VIEW if exists _export_log_fatal_in_progress_export_processed_tasks_view;
 
 CREATE OR REPLACE VIEW _export_log_fatal_in_progress_export_processed_tasks_view AS
 WITH export_log_fatal_in_progress_export_processed_tasks AS (
-    SELECT COUNT(*) AS count
+    SELECT COUNT(*) AS count,
+    status as status
     FROM export_logs
     WHERE
         workspace_id IN (SELECT id FROM prod_workspaces_view)
         AND status IN ('FATAL', 'IN_PROGRESS', 'EXPORT_PROCESSED')
         AND updated_at < NOW() - INTERVAL '45 mins'
+    GROUP BY status
 )
 SELECT * FROM export_log_fatal_in_progress_export_processed_tasks;
--- qbd/views/alerts/add_request_queue_fatal_in_progress_export_processed_tasks_view.sql
+++ qbd/views/alerts/add_request_queue_fatal_in_progress_export_processed_tasks_view.sql
@@ -2,11 +2,13 @@ DROP VIEW IF EXISTS add_queue_fatal_in_progress_export_processed_tasks_view;
 
 CREATE OR REPLACE VIEW add_queue_fatal_in_progress_export_processed_tasks_view AS 
 WITH add_request_queue_fatal_in_progress_export_processed_tasks AS (
-    SELECT COUNT(*) AS count
+    SELECT COUNT(*) AS count,
+    status as status
     FROM qbd_add_request_queue
     WHERE
         workspace_id IN (SELECT id FROM prod_workspaces_view)
         AND status IN ('FATAL', 'IN_PROGRESS', 'EXPORT_PROCESSED')
         AND updated_at BETWEEN NOW() - INTERVAL '24 hours' AND NOW() - INTERVAL '45 mins'
+    GROUP BY status
 )
 SELECT * FROM add_request_queue_fatal_in_progress_export_processed_tasks;
