fdc353944bf55d39c41c423e2885da5d7c0fa9f2
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-03-25 20:02:00 +0530

    Feat: Add helper queries for adding replication idenity and adding tables to publication

---
+++ integrations_settings/helpers/add-tables-to-publication.sql
@@ -0,0 +1,23 @@
+DO
+    $$
+    DECLARE
+        tables_list text;
+    BEGIN
+        SELECT string_agg(quote_ident(tablename), ', ')
+        INTO tables_list
+        FROM pg_tables
+        WHERE schemaname = 'public'
+        AND tablename NOT IN (
+            'django_admin_log', 
+            'django_content_type', 
+            'django_migrations',
+            'django_q_ormq', 
+            'django_q_schedule', 
+            'django_q_task', 
+            'django_session',
+            'update_logs',
+        );
+
+        EXECUTE 'CREATE PUBLICATION events FOR TABLE ' || tables_list;
+END
+$$;
