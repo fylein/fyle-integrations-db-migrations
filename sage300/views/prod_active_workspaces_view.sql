e4cc855748031fd7cc8eef50df7d7cf24910f363
ashwin1111 <winash1998@gmail.com>
2025-04-08 15:30:56 +0530

    Update prod_active_workspaces_view in sage_desktop

--- sage300/views/prod_active_workspaces_view.sql
+++ sage300/views/prod_active_workspaces_view.sql
@@ -6,7 +6,7 @@ SELECT w.*,
    FROM workspaces w
      JOIN workspaces_user wu ON wu.workspace_id = w.id
      JOIN users u ON u.id = wu.user_id
-  WHERE u.email !~* '%fyle%' AND (w.id IN ( SELECT DISTINCT accounting_exports.workspace_id
+  WHERE u.email !~* 'fyle' AND (w.id IN ( SELECT DISTINCT accounting_exports.workspace_id
            FROM accounting_exports
-          WHERE accounting_exports.status = 'COMPLETE' AND accounting_exports.type !~* <> 'FETCHING_' AND accounting_exports.type::text <> ''::text AND accounting_exports.updated_at > (now() - '3 mons'::interval)))
+          WHERE accounting_exports.status = 'COMPLETE' AND accounting_exports.type !~* 'FETCHING_' AND accounting_exports.type::text <> ''::text AND accounting_exports.updated_at > (now() - '3 mons'::interval)))
   GROUP BY w.id;
