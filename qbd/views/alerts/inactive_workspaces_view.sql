0e8359d1e0dfd774984c5d940ddb7fca9b6f008a
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-06-12 23:16:59 +0530

    fix: inactive org view for qbd

--- qbd/views/alerts/inactive_workspaces_view.sql
+++ qbd/views/alerts/inactive_workspaces_view.sql
@@ -5,9 +5,16 @@ SELECT
     COUNT(*), 
     current_database() AS database
 FROM 
-    workspaces
+    workspaces w
+JOIN 
+    export_summary es 
+    ON w.id = es.workspace_id
+JOIN 
+    django_q_schedule dqs 
+    ON w.id::text = dqs.args
 WHERE
-    source_synced_at < NOW() - INTERVAL '2 months'
-    AND reimbursable_last_synced_at < NOW() - INTERVAL '2 months'
-    AND credit_card_last_synced_at < NOW() - INTERVAL '2 months'
-    AND id IN (SELECT id FROM prod_workspaces_view);
+    w.source_synced_at < NOW() - INTERVAL '2 months'
+    AND w.reimbursable_last_synced_at < NOW() - INTERVAL '2 months'
+    AND w.credit_card_last_synced_at < NOW() - INTERVAL '2 months'
+    AND es.last_exported_at < NOW() - INTERVAL '2 months'
+    AND w.id IN (SELECT id FROM prod_workspaces_view);
