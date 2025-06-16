1766e0c4a19021e2c83992340d4542c29581d704
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2025-06-16 14:21:42 +0530

    fix: accounting export synced count (#36)

--- common/qbo_intacct_netsuite_xero/views/alerts/_direct_export_errored_expenses_view_qbo_xero.sql
+++ common/qbo_intacct_netsuite_xero/views/alerts/_direct_export_errored_expenses_view_qbo_xero.sql
@@ -1,13 +1,12 @@
-DROP VIEW IF EXISTS direct_export_errored_expenses_view;
+DROP VIEW IF EXISTS _direct_export_errored_expenses_view;
 
-CREATE VIEW direct_export_errored_expenses_view AS
+CREATE VIEW _direct_export_errored_expenses_view AS
 WITH prod_workspace_ids AS (
     SELECT id
     FROM prod_workspaces_view
 ),
--- Task log in complete state but accounting export summary is not in complete state
 errored_expenses_in_complete_state AS (
-    SELECT COUNT(*) AS complete_expenses_error_count
+    SELECT COUNT(*)  AS complete_expenses_error_count
     FROM expenses
     WHERE
         workspace_id IN (SELECT id FROM prod_workspace_ids)
@@ -20,12 +19,9 @@ errored_expenses_in_complete_state AS (
                 FROM task_logs
                 WHERE status = 'COMPLETE'
                 AND workspace_id IN (SELECT id FROM prod_workspace_ids)
-                AND updated_at > (NOW() - INTERVAL '1 day') 
-                AND updated_at < (NOW() - INTERVAL '45 mins')
             )
         )
 ),
--- Task log in error, fatal state but accounting export summary is not in error state
 errored_expenses_in_error_state AS (
     SELECT COUNT(*) AS error_expenses_error_count
     FROM expenses
@@ -40,12 +36,9 @@ errored_expenses_in_error_state AS (
                 FROM task_logs
                 WHERE status IN ('FAILED', 'FATAL')
                 AND workspace_id IN (SELECT id FROM prod_workspace_ids)
-                AND updated_at > (NOW() - INTERVAL '1 day') 
-                AND updated_at < (NOW() - INTERVAL '45 mins')
             )
         )
 ),
--- Task log in in progress state but accounting export summary is not in in progress state
 errored_expenses_in_inprogress_state AS (
     SELECT COUNT(*) AS in_progress_expenses_error_count
     FROM expenses
@@ -58,21 +51,19 @@ errored_expenses_in_inprogress_state AS (
             WHERE expensegroup_id IN (
                 SELECT expense_group_id
                 FROM task_logs
-                WHERE status in ('IN_PROGRESS', 'ENQUEUED')
+                WHERE status = 'IN_PROGRESS'
                 AND workspace_id IN (SELECT id FROM prod_workspace_ids)
-                AND updated_at > (NOW() - INTERVAL '1 day') 
-                AND updated_at < (NOW() - INTERVAL '45 mins')
             )
         )
 ),
 not_synced_to_platform AS (
     SELECT COUNT(*) AS not_synced_expenses_count
-    FROM expenses
+    FROM expenses e
+    INNER JOIN workspace_general_settings wgs on e.workspace_id = wgs.workspace_id
     WHERE
         workspace_id IN (SELECT id FROM prod_workspace_ids)
         AND accounting_export_summary->>'synced' = 'false'
-        AND updated_at > (NOW() - INTERVAL '1 day') 
-        AND updated_at < (NOW() - INTERVAL '45 mins')
+        AND wgs.skip_accounting_export_summary_post = 'false'
 )
 SELECT 
     complete_expenses_error_count,
