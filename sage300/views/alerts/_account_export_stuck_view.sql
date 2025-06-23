b85b7a87359b6fb1f64a45bf4155000273c60895
Hrishabh Tiwari <hrishabh.t@fyle.in>
2025-06-23 21:31:39 +0530

    fix: sage 300 alert view

--- sage300/views/alerts/_account_export_stuck_view.sql
+++ sage300/views/alerts/_account_export_stuck_view.sql
@@ -2,11 +2,12 @@ DROP VIEW IF EXISTS _account_export_stuck_view;
 
 CREATE OR REPLACE VIEW _account_export_stuck_view AS
 SELECT
-    workspace_id,
+    workspace_id as workspace_id,
     COUNT(*) AS count
 FROM
     accounting_exports
 WHERE
     workspace_id IN (SELECT id FROM prod_workspaces_view)
     AND status in ('IN_PROGRESS', 'EXPORT_QUEUED')
+    AND type !~* 'fetching'
 GROUP BY workspace_id;
--- sage300/views/alerts/account_export_stuck_view.sql
+++ sage300/views/alerts/account_export_stuck_view.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS account_export_stuck_view;
 
 CREATE OR REPLACE VIEW account_export_stuck_view AS
 SELECT
-    workspace_id,
+    workspace_id as workspace_id,
     COUNT(*) AS count
 FROM
     accounting_exports
@@ -10,4 +10,5 @@ WHERE
     workspace_id IN (SELECT id FROM prod_workspaces_view)
     AND status in ('IN_PROGRESS', 'EXPORT_QUEUED')
     AND updated_at BETWEEN NOW() - INTERVAL '24 hours' AND NOW() - INTERVAL '30 mins'
+    AND type !~* 'fetching'
 GROUP BY workspace_id;
