8e925a30438d3b13929f6e2bbbb9b984b2c2f1a4
Ashutosh singh <ashutoshsinghrkt@gmail.com>
2025-11-11 21:29:28 +0530

    Fix: Update create_request_queue_fatal_in_progress_response_received_tasks view to ignore some errors (#57)
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- qbd/views/alerts/create_request_queue_fatal_in_progress_response_received_tasks_view.sql
+++ qbd/views/alerts/create_request_queue_fatal_in_progress_response_received_tasks_view.sql
@@ -9,6 +9,7 @@ WITH create_request_queue_fatal_in_progress_response_received_tasks AS (
         workspace_id IN (SELECT id FROM prod_workspaces_view)
         AND status IN ('FATAL', 'IN_PROGRESS', 'RESPONSE_RECEIVED')
         AND updated_at BETWEEN NOW() - INTERVAL '24 hours' AND NOW() - INTERVAL '45 mins'
+        AND NOT (object_type = 'VENDOR' AND response::text ILIKE '%of the list element is already in use%')
     GROUP BY status
 )
 SELECT * FROM create_request_queue_fatal_in_progress_response_received_tasks;
