964c4c6caf8e7c19c41dde2f29a892196067cf08
ruuushhh <rushikeshtayade2001@gmail.com>
2025-12-17 16:39:23 +0530

    fix

--- common/global_shared/triggers/add-tables-to-publication.sql
+++ common/global_shared/triggers/add-tables-to-publication.sql
@@ -1,35 +1,55 @@
-CREATE OR REPLACE FUNCTION add_tables_to_publication()
+CREATE OR REPLACE FUNCTION public.add_tables_to_publication()
 RETURNS event_trigger AS $$
+DECLARE
+    obj record;
+    schema_name text;
+    table_name text;
 BEGIN
-    IF TG_EVENT = 'ddl_command_end' THEN
-        IF object_type = 'table' AND schema_name = 'public' THEN
-            IF NEW.table_name NOT IN (
-                'django_admin_log',
-                'django_content_type',
-                'django_migrations',
-                'django_q_ormq',
-                'django_q_schedule',
-                'django_q_task',
-                'django_session'
-            ) THEN
-                EXECUTE format(
-                    'ALTER PUBLICATION events ADD TABLE %I.%I',
-                    schema_name,
-                    NEW.table_name
-                );
-                
-                EXECUTE format(
-                    'ALTER TABLE %I.%I REPLICA IDENTITY FULL',
-                    schema_name,
-                    NEW.table_name
-                );
-            END IF;
+    FOR obj IN 
+        SELECT * FROM pg_event_trigger_ddl_commands()
+        WHERE command_tag = 'CREATE TABLE'
+    LOOP
+        RAISE NOTICE 'Processing new table: %', obj.object_identity;
+        --- The format of the object_identity is schema.table
+        schema_name := split_part(obj.object_identity, '.', 1);
+        table_name := split_part(obj.object_identity, '.', 2);
+
+        -- Skip if not in public schema
+        IF schema_name <> 'public' THEN
+            CONTINUE;
         END IF;
-    END IF;
+
+        -- Skip excluded system tables
+        IF table_name IN (
+            'django_admin_log', 
+            'django_content_type', 
+            'django_migrations',
+            'django_q_ormq', 
+            'django_q_schedule', 
+            'django_q_task', 
+            'django_session',
+            'expense_attributes_deletion_cache'
+        ) THEN
+            RAISE NOTICE 'Skipping excluded table: %.%', schema_name, table_name;
+            CONTINUE;
+        END IF;
+
+        RAISE NOTICE 'Processing new table: %.%', schema_name, table_name;
+
+        -- Set REPLICA IDENTITY FULL
+        EXECUTE format('ALTER TABLE %I.%I REPLICA IDENTITY FULL', schema_name, table_name);
+
+        -- Add to publication (ignore duplicates)
+        BEGIN
+            EXECUTE format('ALTER PUBLICATION events ADD TABLE %I.%I', schema_name, table_name);
+        EXCEPTION WHEN duplicate_object THEN
+            RAISE NOTICE 'Table %.% already in publication.', schema_name, table_name;
+        END;
+    END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE EVENT TRIGGER publication_table_trigger
-ON ddl_command_end
+CREATE EVENT TRIGGER on_table_created
+ON ddl_command_end                        
 WHEN TAG IN ('CREATE TABLE')
-EXECUTE FUNCTION add_tables_to_publication();
+EXECUTE FUNCTION public.add_tables_to_publication();
